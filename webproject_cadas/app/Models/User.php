e917c480ab5672af3a4fb483a03fd53d4149e14b
AmishaLamichhane <amisha.lamichhane1@gmail.com>
2024-09-17 13:57:37 +0545

    Filament User Updated
    error solved.

--- webproject_cadas/app/Models/User.php
+++ webproject_cadas/app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+
 
 class User extends Authenticatable implements FilamentUser
 {
