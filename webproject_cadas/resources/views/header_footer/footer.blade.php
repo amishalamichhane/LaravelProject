6452f372c11e4e48cd7880deb7703a1d7d8ed9a5
AmishaLamichhane <amisha.lamichhane1@gmail.com>
2024-09-17 13:43:35 +0545

    Footer update
    Change in footer content.

--- webproject_cadas/resources/views/header_footer/footer.blade.php
+++ webproject_cadas/resources/views/header_footer/footer.blade.php
@@ -13,22 +13,21 @@
             FAMELO | University of Michigan<br>
             Michigan MI 94720-2120<br>
             E-mail: iser.nepal@outlook.com<br><br>
-            <p><a href="https://umich.edu/">UCB Privacy Statement <i class="fa fa-external-link"
-                        aria-hidden="true"></i></a>| <a
-                    href="https://dap.berkeley.edu/get-help/report-web-accessibility-issue">Web Accessibility Policy</a>
-                <i class="fa fa-external-link" aria-hidden="true"></i>| Disclaimer
-            </p>
 
         </div>
 
-        <div class="right_side" align="right">
-            {{-- <img src="/img/isernepal-at.png"> --}}
+        <div class="right_side">
 
             <p><a href="https://umich.edu/">University of Michigan, Michigan </a><i class="fa fa-external-link"
                 aria-hidden="true"></i> | <a href=" ">ISER-N </a><i class="fa fa-external-link"
                 aria-hidden="true"></i>
             <br>This project is funded by the University of Michigan.
-        </p>
+
+        </p><p><a href="https://umich.edu/">UCB Privacy Statement <i class="fa fa-external-link"
+                        aria-hidden="true"></i></a>| <a
+                    href="https://dap.berkeley.edu/get-help/report-web-accessibility-issue">Web Accessibility Policy</a>
+                <i class="fa fa-external-link" aria-hidden="true"></i>| Disclaimer
+            </p>
 
 
         </div>
