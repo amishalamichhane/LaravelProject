7980d4eb5df23573ad281763872cbcd5b6c99446
AmishaLamichhane <amisha.lamichhane1@gmail.com>
2024-09-18 11:22:13 +0545

    Footer modified
    Copyright aligned to center.

--- webproject_cadas/resources/views/header_footer/footer.blade.php
+++ webproject_cadas/resources/views/header_footer/footer.blade.php
@@ -9,7 +9,7 @@
                 <h4><br>Family Migration<br> and early life<br> outcomes</h4>
                 <br>
             </div>
-            
+
             FAMELO | University of Michigan<br>
             Michigan MI 94720-2120<br>
             E-mail: iser.nepal@outlook.com<br><br>
@@ -18,15 +18,16 @@
 
         <div class="right_side">
             <div class="right_side_logo">
-            <img src="/img/iser.png" alt="ISERN_N Logo" class="img-fluid">
-            <img src="/img/michigan-M.png" alt="Michigan Logo" class="img-fluid">
+                <img src="/img/iser.png" alt="ISERN_N Logo" class="img-fluid">
+                <img src="/img/michigan-M.png" alt="Michigan Logo" class="img-fluid">
             </div>
             <p><a href="https://umich.edu/"><br>University of Michigan, Michigan </a><i class="fa fa-external-link"
-                aria-hidden="true"></i> | <a href=" ">ISER-N </a><i class="fa fa-external-link"
-                aria-hidden="true"></i>
-            <br>This project is funded by the University of Michigan.
+                    aria-hidden="true"></i> | <a href=" ">ISER-N </a><i class="fa fa-external-link"
+                    aria-hidden="true"></i>
+                <br>This project is funded by the University of Michigan.
 
-        </p><p><a href="https://umich.edu/">UCB Privacy Statement <i class="fa fa-external-link"
+            </p>
+            <p><a href="https://umich.edu/">UCB Privacy Statement <i class="fa fa-external-link"
                         aria-hidden="true"></i></a>| <a
                     href="https://dap.berkeley.edu/get-help/report-web-accessibility-issue">Web Accessibility Policy</a>
                 <i class="fa fa-external-link" aria-hidden="true"></i>| Disclaimer
@@ -34,12 +35,14 @@
 
 
         </div>
-        <hr>
-        <div class="mid" >
-             <p>© Institute for Social and Environmental Research - Nepal | website: <a href="https://isernepal.org.np/
-                ">ISER-N</a>{{-- | <a href="https://davidkerrdesign.com/">website: David Kerr Design</a><i
-                    class="fa fa-external-link" aria-hidden="true"></i>--}}</p> 
-        </div>
+    </div>
 
+    <hr>
+
+    <div class="mid">
+        <p>© Institute for Social and Environmental Research - Nepal | website: <a
+                href="https://isernepal.org.np/
+            ">ISER-N</a>{{-- | <a href="https://davidkerrdesign.com/">website: David Kerr Design</a><i
+                class="fa fa-external-link" aria-hidden="true"></i> --}}</p>
     </div>
 </footer>
