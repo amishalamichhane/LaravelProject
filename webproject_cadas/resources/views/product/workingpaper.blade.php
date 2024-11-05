771838791f15df06577c005eea0de95e50022965
AmishaLamichhane <amisha.lamichhane1@gmail.com>
2024-11-05 11:08:54 +0545

    Update workingpaper.blade.php
    Updated working paper to add details from the backend.

--- webproject_cadas/resources/views/product/workingpaper.blade.php
+++ webproject_cadas/resources/views/product/workingpaper.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
     <div class="working-paper-container">
-        <h1>Working Papers</h1>
+        <h1><b>Working Papers</b></h1>
 
         @foreach($workingPapers as $paper)
             <div class="paper-card">
-                <h2>Title: {{ $paper->title }}</h2>
-                <p class="author">Authors: {{ $paper->authors }}</p>
-                <p class="date">Date: {{ $paper->date->format('F Y') }}</p>
+                <h2> {{ $paper->title }}</h2>
+                {{-- <p class="author">Authors: {{ $paper->authors }}</p> --}}
+                {{-- <p class="date">Date: {{ $paper->date->format('F Y') }}</p> --}}
                 <p class="summary">{{ $paper->summary }}</p>
-                <a href="{{ asset($paper->file_path) }}" class="button">Download Full Paper</a>
+                <a href="{{ asset($paper->file_path) }}" target="_blank" class="button">Download Full Paper</a>
             </div>
         @endforeach
     </div>
