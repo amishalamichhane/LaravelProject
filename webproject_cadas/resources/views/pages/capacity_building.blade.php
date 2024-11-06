7d9f56f95dfe0adfd2d8ab06dce209c3ff440e10
AmishaLamichhane <amisha.lamichhane1@gmail.com>
2024-11-06 12:52:18 +0545

    Update capacity_building.blade.php

--- webproject_cadas/resources/views/pages/capacity_building.blade.php
+++ webproject_cadas/resources/views/pages/capacity_building.blade.php
@@ -1,11 +1,29 @@
 @extends('layout')
 
 @section('content')
-    <div class="container">
+    <div class="container_capacitybuilding">
         @foreach ($capacity_building as $section)
             <div class="capacity-section">
-                <h2>{{ $section->section_title }}</h2>
-                <p>{{ $section->section_content }}</p>
+                <div class="text-section">
+                    <h2>{{ $section->section_title }}</h2>
+                    <p>{{ Str::limit($section->section_content, 200)}}<a href="{{ route('capacity_building', $section->slug) }}">Read More</a></p>
+
+                    <!-- "Read More" link to the show page -->
+                    
+                </div>
+                
+                {{-- <div class="image-gallery">
+                    @php
+                        $imageUrls = json_decode($section->image_urls);
+                    @endphp
+                    @if (is_array($imageUrls) && count($imageUrls) > 0)
+                        @foreach ($imageUrls as $image_url)
+                            <img src="{{ asset($image_url) }}" alt="Image" class="capacity-image" />
+                        @endforeach
+                    @else
+                        <p>No images available.</p>
+                    @endif
+                </div> --}}
             </div>
         @endforeach
     </div>
