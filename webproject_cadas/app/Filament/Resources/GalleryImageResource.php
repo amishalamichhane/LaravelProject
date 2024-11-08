0438ac915ef2a6e87639ef03fee646dede04b288
AmishaLamichhane <amisha.lamichhane1@gmail.com>
2024-11-08 10:21:21 +0545

    update model for gallery

---
+++ webproject_cadas/app/Filament/Resources/GalleryImageResource.php
@@ -0,0 +1,66 @@
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\GalleryImageResource\Pages;
+use App\Filament\Resources\GalleryImageResource\RelationManagers;
+use App\Models\GalleryImage;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class GalleryImageResource extends Resource
+{
+    protected static ?string $model = GalleryImage::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('img url')->required(),
+                Forms\Components\Select::make('gallery_id')
+                    ->relationship('gallery', 'title') // Link to gallery titles
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListGalleryImages::route('/'),
+            'create' => Pages\CreateGalleryImage::route('/create'),
+            'edit' => Pages\EditGalleryImage::route('/{record}/edit'),
+        ];
+    }
+}
