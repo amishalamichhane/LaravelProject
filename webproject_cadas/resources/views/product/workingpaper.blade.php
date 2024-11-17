485f12149c0dfadcd3ae14c116308ca30448bfde
AmishaLamichhane <amisha.lamichhane1@gmail.com>
2024-11-17 12:07:55 +0545

    update workingpaper.blade.php

--- webproject_cadas/resources/views/product/workingpaper.blade.php
+++ webproject_cadas/resources/views/product/workingpaper.blade.php
@@ -3,8 +3,18 @@
 @section('content')
     <div class="working-paper-container">
         <h1><b>Working Papers</b></h1>
+        <div>
+            <p>
+                The Working Papers section contains preliminary research outputs that are still in development. These papers are
+            part of an ongoing effort to explore key questions in the field of family migration and its impact on children's
+            socio-psychological competence. Shared here for the purpose of gathering feedback, these papers aim to engage
+            with the wider academic and research community, fostering collaboration and further development of ideas.
 
-        @foreach($workingPapers as $paper)
+            </p>
+
+        </div>
+
+        @foreach ($workingPapers as $paper)
             <div class="paper-card">
                 <h2> {{ $paper->title }}</h2>
                 {{-- <p class="author">Authors: {{ $paper->authors }}</p> --}}
